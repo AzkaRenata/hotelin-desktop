47667c978bc11540ae95bcdd5f5546c08a65b351
Muhammad Mujir <muhammadmujir011@gmail.com>
2020-12-06 13:01:37 +0700

    room detail, view and delete room list

---
+++ Model/RoomDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelin_Desktop.Model
+{
+    public class RoomDetail
+    {
+        public Hotel hotel { get; set; }
+        public Room room { get; set; }
+        public List<RoomFacility> facility { get; set; }
+    }
+}
