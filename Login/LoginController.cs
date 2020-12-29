59f670c38b5e4d85903c758379f492a9d5454e76
adam141100 <adampambudi141100@gmail.com>
2020-12-29 17:31:24 +0700

    Validasi Login
    Done

--- Login/LoginController.cs
+++ Login/LoginController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Velacro.Api;
 using Velacro.Basic;
 
@@ -32,6 +33,14 @@ namespace Hotelin_Desktop.Login
 
         }
 
+        private bool validateLoginInput(string _email, string _password)
+        {
+            if (_email.Length == 0) return false;
+            if (_password.Length == 0) return false;
+
+            return true;
+        }
+
         private void setTokenStatus(HttpResponseBundle _response)
         {
             if (_response.getHttpResponseMessage().Content != null)
@@ -42,19 +51,36 @@ namespace Hotelin_Desktop.Login
 
         public async void login(string _email, string _password)
         {
-            var client = new ApiClient(MyURL.MyURL.baseURL);
-            var request = new ApiRequestBuilder();
+            if(validateLoginInput(_email, _password))
+            {
+                var client = new ApiClient(MyURL.MyURL.baseURL);
+                var request = new ApiRequestBuilder();
 
-            var req = request
-                .buildHttpRequest()
-                .addParameters("email", _email)
-                .addParameters("password", _password)
-                .setEndpoint("user/login")
-                .setRequestMethod(HttpMethod.Post);
-            client.setOnSuccessRequest(setViewLoginStatus);
-            var response = await client.sendRequest(request.getApiRequestBundle());
-            Console.WriteLine("token : " + response.getJObject()["token"]);
-            //client.setAuthorizationToken(response.getJObject()["access_token"].ToString());
+                var req = request
+                    .buildHttpRequest()
+                    .addParameters("email", _email)
+                    .addParameters("password", _password)
+                    .setEndpoint("user/login")
+                    .setRequestMethod(HttpMethod.Post);
+                client.setOnSuccessRequest(setViewLoginStatus);
+                client.setOnFailedRequest(onFailedRequest);
+                var response = await client.sendRequest(request.getApiRequestBundle());
+                // Console.WriteLine("token : " + response.getJObject()["token"]);
+                //client.setAuthorizationToken(response.getJObject()["access_token"].ToString());
+            }
+            else
+            {
+                MessageBox.Show("One of the required field is empty. Please try again.", "Login Failed");
+                // Console.WriteLine("One of the required field is empty. Please try again.");
+            }
+
+            
+        }
+
+        private void onFailedRequest(HttpResponseBundle _response)
+        {
+            MessageBox.Show("Email-Password combination doesn't match. Please try again.", "Login Failed");
+            //Console.WriteLine("Email-Password combination doesn't match. Please try again");
         }
 
         private void setViewLoginStatus(HttpResponseBundle _response)
@@ -62,8 +88,13 @@ namespace Hotelin_Desktop.Login
             if (_response.getHttpResponseMessage().Content != null)
             {
                 string status = _response.getHttpResponseMessage().ReasonPhrase;
+
+                int statusCode = (int) _response.getHttpResponseMessage().StatusCode;
+
+                Console.WriteLine(_response.getHttpResponseMessage());
                 string token = _response.getJObject()["token"].ToString();
                 getView().callMethod("saveToken", token);
+
                 //getView().callMethod("tesPrint");
                 //getView().callMethod("moveToDashboard");
             }
