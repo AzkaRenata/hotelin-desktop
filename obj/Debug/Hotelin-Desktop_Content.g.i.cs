b8b669bba8a94969171b915039e7be42d1ec9c14
sarahafifah <sarahafifah109@gmail.com>
2020-12-30 21:13:39 +0700

    Revisi Detail Booking

--- obj/Debug/Hotelin-Desktop_Content.g.i.cs
+++ obj/Debug/Hotelin-Desktop_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("profile/default.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("detailpengunjung/profile.jpg")]
 
 
